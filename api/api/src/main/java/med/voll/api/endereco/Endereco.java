6b47af089e27cbe36ffb06b7af1726c210a3c659
renataaline <renataaline@sefaz.pi.gov.br>
2024-08-30 11:58:54 -0300

    Alteração no controller e criação de migration

--- api/api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -22,4 +22,14 @@ public class Endereco {
     private String uf;
 
 
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.Logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+        this.cidade = dados.cidade();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+
+    }
 }
