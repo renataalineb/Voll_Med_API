8f844df1882c920f91a76c033b7a4f0c10cf3561
renataalineb <renataalinebv@gmail.com>
2024-10-07 20:50:11 -0300

    Classes de Validação

--- api/api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ api/api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -26,4 +26,11 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
            limit 1
             """)
     Medico escolherMedicoAleatorioLivrenaData(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            select m.ativo
+            from Medico m
+            where m.id = :id
+            """)
+    Boolean findAtivoById(Long idMedico);
 }
