a006da909faf4236a8a4a1fa6879669d102b49ec
renataalineb <renataalinebv@gmail.com>
2024-10-07 22:15:39 -0300

    Implementação do algoritmo de cancelamento de consultas e suas validações.

--- api/api/src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorHorarioAntecedencia.java
+++ api/api/src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorHorarioAntecedencia.java
@@ -1,14 +1,13 @@
-package med.voll.api.domain.consulta.validacoes;
+package med.voll.api.domain.consulta.validacoes.agendamento;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-@Component
+@Component("ValidadorHorarioAntecedenciaAgendamento")
 public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoDeConsulta{
     public void validar(DadosAgendamentoConsulta dados){
         var dataConsulta = dados.data();
